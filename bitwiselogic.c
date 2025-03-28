46d41b55f5f2c6d081671f18ff2128d21dd33327
ChessGame <83489711+ChessGame1900@users.noreply.github.com>
2025-03-28 20:36:21 +0200

    Update bitwiselogic.c
    fixed negative number by using two's complement( removed some functions( multiplication and others))

--- bitwiselogic.c
+++ bitwiselogic.c
@@ -15,62 +15,50 @@ void printResult(int result, int operation);
 void printDisplay(int result, int operation);
 int binary(char binaryString[]);
 
+int getRegisterValue() {
+    char input[33];
+    printf("Enter register value (binary or decimal): \n");
+    scanf("%s", input);
+
+    int isBinary = 1;
+    for (int i = 0; input[i] != '\0'; i++) {
+        if (input[i] != '0' && input[i] != '1') {
+            isBinary = 0;
+            break;
+        }
+    }
+
+    return isBinary ? binary(input) : atoi(input);
+}
+
 int main(void) {
     int operation;
     struct registers A;
-    char input[33];
-    int isBinary = 1;
 
-    printf("Enter register1: \n");
-    scanf("%s", input);
-
-    for (int i = 0; input[i] != '\0'; i++) {
-        if (input[i] != '0' && input[i] != '1') {
-            isBinary = 0;
-            break;
-        }
-    }
-    A.register1 = isBinary ? binary(input) : atoi(input);
-
-    printf("Enter register2:\n ");
-    scanf("%s", input);
-
-    isBinary = 1;
-    for (int i = 0; input[i] != '\0'; i++) {
-        if (input[i] != '0' && input[i] != '1') {
-            isBinary = 0;
-            break;
-        }
-    }
-    A.register2 = isBinary ? binary(input) : atoi(input);
+    A.register1 = getRegisterValue();
+    A.register2 = getRegisterValue();
 
     printf("Enter operation:\n");
-    printf("1=AND, 2=OR, 3=XOR, 4=NOR, 5=NOT, 6=SHL, 7=SHR, 8=ADD, 9=subtraction,10=multiplication,11=division\n");
+    printf("1=AND, 2=OR, 3=XOR, 4=NOR: \n");
     scanf("%d", &operation);
 
-    if (operation >= 1 && operation <= 11) { 
+    if (operation >= 1 && operation <= 4) {
         int result = performOperation(A, operation);
         printResult(result, operation);
         printDisplay(result, operation);
     } else {
         printf("Invalid operation!\n");
     }
+
     return 0;
 }
 int performOperation(struct registers A, int operation) {
     int result = 0;
     switch (operation) {
-        case 1: result = A.register1 & A.register2; break; 
-        case 2: result = A.register1 | A.register2; break; 
-        case 3: result = A.register1 ^ A.register2; break; 
-        case 4: result = ~(A.register1 | A.register2); break; 
-        case 5: result = ~A.register1; break; 
-        case 6: result = A.register1 << A.register2; break; 
-        case 7: result = A.register1 >> A.register2; break; 
-        case 8: result = A.register1 + A.register2; break; 
-        case 9: result =  A.register1 - A.register2; break; 
-        case 10:result =  A.register1  *A.register2; break; 
-         case 11: result =  A.register1 / A.register2; break; 
+        case 1: result = A.register1 & A.register2; break;
+        case 2: result = A.register1 | A.register2; break;
+        case 3: result = A.register1 ^ A.register2; break;
+        case 4: result = ~(A.register1 | A.register2); break;
     }
     return result;
 }
@@ -80,20 +68,12 @@ void printResult(int result, int operation) {
         case 2: printf("Result of OR: %d\n", result); break;
         case 3: printf("Result of XOR: %d\n", result); break;
         case 4: printf("Result of NOR: %d\n", result); break;
-        case 5: printf("Result of NOT: %d\n", result); break;
-        case 6: printf("Result of SHL: %d\n", result); break;
-        case 7: printf("Result of SHR: %d\n", result); break;
-        case 8: printf("Result of  addition: %d\n", result); break;
-        case 9: printf("Result of subtraction: %d\n", result); break;
-        case 10: printf("Result of multiplication: %d\n", result); break;
-        case 11: printf("Result of division: %d\n", result); break;
     }
 }
 void printDisplay(int result, int operation) {
     int digits[10];
     int numDigits = 0;
     int temp = abs(result);
-
     if (temp == 0) {
         digits[0] = 0;
         numDigits = 1;
@@ -110,13 +90,13 @@ void printDisplay(int result, int operation) {
     for (int i = numDigits - 1; i >= 0; i--) {
         switch (digits[i]) {
             case 0: printf(" _ \n| |\n|_|\n\n"); break;
-            case 1: printf("  \n |\n |\n\n"); break;
+            case 1: printf("    \n  |\n  |\n\n"); break;
             case 2: printf(" _ \n _|\n|_ \n\n"); break;
             case 3: printf(" _ \n _|\n _|\n\n"); break;
-            case 4: printf("  \n|_|\n |\n\n"); break;
+            case 4: printf("    \n|_|\n  |\n\n"); break;
             case 5: printf(" _ \n|_ \n _|\n\n"); break;
             case 6: printf(" _ \n|_ \n|_|\n\n"); break;
-            case 7: printf(" _ \n |\n |\n\n"); break;
+            case 7: printf(" _ \n  |\n  |\n\n"); break;
             case 8: printf(" _ \n|_|\n|_|\n\n"); break;
             case 9: printf(" _ \n|_|\n _|\n\n"); break;
             default: printf("?\n");
@@ -125,20 +105,25 @@ void printDisplay(int result, int operation) {
 }
 int binary(char binaryString[]) {
     int decimalValue = 0;
-    int length = strlen(binaryString);
-    for (int i = 0; i < length; i++) {
+    int length, i, power;
+    length = strlen(binaryString);
+    for (i = 0; i < length; i++) {
         if (binaryString[i] == '1') {
-            decimalValue = (decimalValue << 1) | 1;
-        } else {
-            decimalValue = decimalValue << 1;
+            power = length - i - 1;
+            decimalValue += (int)pow(2, power);
         }
     }
     printf("press 0 for negative value, press  1  for positive, press 2 to quit \n");
     int sign;
     scanf("%d", &sign);
     switch (sign) {
-        case 0: decimalValue = -1 * decimalValue; break;
-        case 2: exit(10);
+        case 0:
+            if (decimalValue != 0) {
+                decimalValue = ~decimalValue + 1;
+            }
+            break;
+        case 2:
+            exit(10);
     }
     return decimalValue;
 }
