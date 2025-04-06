ae56f598c250227c1be8ef5b9703f11c49a9ffab
ChessGame <83489711+ChessGame1900@users.noreply.github.com>
2025-04-06 13:57:29 +0300

    Update bitwiselogic.c
    some fixes( work in progress)

--- bitwiselogic.c
+++ bitwiselogic.c
@@ -24,22 +24,25 @@ void functions(struct registers *reg);
 int main(void) {
     int operation;
     struct registers A;
-
-    A.register1 = getRegisterValue();
-    A.register2 = getRegisterValue();
-
-    printf("Enter operation:\n");
-    printf("1=AND, 2=OR, 3=XOR, 4=NOR: \n");
-    scanf("%d", &operation);
-
-    if (operation >= 1 && operation <= 4) {
-        int result = performOperation(A, operation);
-        printResult(result, operation);
-        printDisplay(result, operation);
-    } else {
-        printf("Invalid operation!\n");
+    int mode;
+    printf("Please input 1 for using cpu emulator or 0 for file coding\n");
+    scanf("%d", &mode);
+    if (mode == 1) {
+        A.register1 = getRegisterValue();
+        A.register2 = getRegisterValue();
+        printf("Enter operation:\n");
+        printf("1=AND, 2=OR, 3=XOR, 4=NOR: \n");
+        scanf("%d", &operation);
+        if (operation >= 1 && operation <= 4) {
+            int result = performOperation(A, operation);
+            printResult(result, operation);
+            printDisplay(result, operation);
+        } else {
+            printf("Invalid operation!\n");
+        }
+    } else if (mode == 0) {
+        functions(&A);
     }
-
     return 0;
 }
 
@@ -55,7 +58,6 @@ int getRegisterValue() {
             break;
         }
     }
-
     return isBinary ? binary(input) : atoi(input);
 }
 
@@ -99,10 +101,10 @@ void printDisplay(int result, int operation) {
     for (int i = numDigits - 1; i >= 0; i--) {
         switch (digits[i]) {
             case 0: printf(" _ \n| |\n|_|\n\n"); break;
-            case 1: printf("    \n  |\n  |\n\n"); break;
+            case 1: printf("   \n  |\n  |\n\n"); break;
             case 2: printf(" _ \n _|\n|_ \n\n"); break;
             case 3: printf(" _ \n _|\n _|\n\n"); break;
-            case 4: printf("    \n|_|\n  |\n\n"); break;
+            case 4: printf("   \n|_|\n  |\n\n"); break;
             case 5: printf(" _ \n|_ \n _|\n\n"); break;
             case 6: printf(" _ \n|_ \n|_|\n\n"); break;
             case 7: printf(" _ \n  |\n  |\n\n"); break;
@@ -139,60 +141,112 @@ int binary(char binaryString[]) {
 }
 
 void variable(char *word[], int wordCount, const char *type, struct registers *reg) {
-    if (wordCount >= 3 && atoi(word[2]) >= 0 &&
-        (strcmp(word[0], "register1") == 0 || strcmp(word[0], "0x0001") == 0 ||
-         strcmp(word[0], "register2") == 0 || strcmp(word[0], "0x0002") == 0 ||
-         strcmp(word[0], "register3") == 0 || strcmp(word[0], "0x0003") == 0)) {
-
-        if (strcmp(type, "int") == 0) {
-            if (strcmp(word[0], "register1") == 0 || strcmp(word[0], "0x0001") == 0) {
-                reg->register1 = atoi(word[2]);
-            } else if (strcmp(word[0], "register2") == 0 || strcmp(word[0], "0x0002") == 0) {
-                reg->register2 = atoi(word[2]);
-            } else if (strcmp(word[0], "register3") == 0 || strcmp(word[0], "0x0003") == 0) {
-                reg->register3 = atoi(word[2]);
+    int regNum = 0;
+    int state;
+    if (strcmp(word[0], "register1") == 0 || strcmp(word[0], "0x0001") == 0) {
+        regNum = 1;
+    } else if (strcmp(word[0], "register2") == 0 || strcmp(word[0], "0x0002") == 0) {
+        regNum = 2;
+    } else if (strcmp(word[0], "register3") == 0 || strcmp(word[0], "0x0003") == 0) {
+        regNum = 3;
+    } else {
+        return; 
+    }
+    if (strcmp(type, "int") == 0) {
+        state = 0;
+        int value = atoi(word[2]);
+        if (value >= 0) {
+            switch (regNum) {
+                case 1:
+                    reg->register1 = value;
+                    break;
+                case 2:
+                    reg->register2 = value;
+                    break;
+                case 3:
+                    reg->register3 = value;
+                    break;
             }
-        } else if (strcmp(type, "string") == 0) {
-            if (strcmp(word[0], "register1") == 0 || strcmp(word[0], "0x0001") == 0) {
+        }
+    } else if (strcmp(type, "string") == 0) {
+        state = 1;
+        switch (regNum) {
+            case 1:
                 strcpy(reg->str1, word[2]);
-            } else if (strcmp(word[0], "register2") == 0 || strcmp(word[0], "0x0002") == 0) {
+                break;
+            case 2:
                 strcpy(reg->str2, word[2]);
-            } else if (strcmp(word[0], "register3") == 0 || strcmp(word[0], "0x0003") == 0) {
+                break;
+            case 3:
                 strcpy(reg->str3, word[2]);
-            }
+                break;
         }
     }
 }
+
+
+if ( strcmp(word[0], "printf")==0  && strcmp(word[1], "register1") == 0 || strcmp(word[1], "0x0001") == 0  )  {
+    if(state==0){
+        printf("%d",register1);
+    }
+    else{
+        printf("%s",str1);
+    }  
+    } else if ( strcmp(word[0], "printf")==0  && strcmp(word[1], "register2") == 0 || strcmp(word[1], "0x0002") == 0  )  {
+    if(state==0){
+        printf("%d",register2);
+    }
+    else{
+        printf("%s",str2);
+    }  
+    } else if ( strcmp(word[0], "printf")==0  && strcmp(word[1], "register3") == 0 || strcmp(word[1], "0x0003") == 0  )  {
+  if(state==0){
+        printf("%d",register3);
+    }
+    else{
+        printf("%s",str3);
+    }  
+    } else {
+        return; 
+    }
 void functions(struct registers *reg) {
     FILE *fptr;
     fptr = fopen("filename.txt", "r");
-    char str[100];
-    char *token;
+    if (fptr == NULL) {
+        printf("Error opening file\n");
+        return;
+    }
+
     printf("Please write limit for string word count: ");
     int x;
     scanf("%d", &x);
-    getchar();
-    char **word = malloc(x * sizeof(char *));
-    if (word == NULL) {
-        printf("Memory allocation failed!\n");
-        return;
-    }
-    fgets(str, 100, fptr);
-    int i = 0;
-    printf("Enter a string: ");
-    token = strtok(str, " ");
-    while (token != NULL && i < x) {
-        word[i] = token;
-        i++;
-        token = strtok(NULL, " ");
-    }
-    fclose(fptr);
-    for (int c = 2; c <= 4; c++) {
-        switch (c) {
-            case 2: variable(word, i, "int", reg); break;
-            case 3: variable(word, i, "bool", reg); break;
-            case 4: variable(word, i, "string", reg); break;
+    getchar(); 
+    printf("Please write limit for line amount: ");
+    int lineLimit;
+    scanf("%d", &lineLimit);
+    getchar(); 
+    char str[100];
+    int lineCount = 0;
+
+    while (fgets(str, 100, fptr) != NULL && lineCount <= lineLimit) {
+        str[strcspn(str, "\n")] = 0; 
+        char **word = malloc(x * sizeof(char *));
+        if (word == NULL) {
+            printf("Memory allocation failed!\n");
+            fclose(fptr);
+            return;
         }
+        int i = 0;
+        char *token = strtok(str, " ");
+        while (token != NULL && i < x) {
+            word[i] = token;
+            i++;
+            token = strtok(NULL, " ");
+        }
+        
+        free(word);
+        lineCount++;
     }
-    free(word);
+
+    fclose(fptr);
 }
